121976c1ef2b8d9ff8108b974f2a1465d3f4d0f5
João Santana <jenryk9503@gmail.com>
2020-02-12 02:51:23 -0300

    Add Album image and Text

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image} from 'react-native';
+import GirlImage from './assets/images/lofi_girl.png'
 import * as Font from 'expo-font'
 export default function App() {
   useEffect(() => {
     Font.loadAsync({
-      'Montserrat' : require('./assets/fonts/Montserrat-Regular.ttf')
+      'Montserrat' : require('./assets/fonts/Montserrat-Regular.ttf'),
+      'Roboto' : require('./assets/fonts/Roboto-Regular.ttf')
     })
   })
   return (
     <View style={styles.container}>
-      <Text style={{color:'snow', fontFamily:'Montserrat'}}>Hello World</Text>
+      <Text style={styles.songTitle}>Song Name</Text>
+      <Image source={GirlImage} style={styles.album}/>
+      <Text style={styles.artistName}>Artist Name</Text>
     </View>
   );
 }
@@ -21,4 +25,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  album: {
+    width: '300px',
+    height: '300px',
+    borderRadius: '150px',
+  },
+  songTitle: {
+    marginBottom: '2%',
+    color: 'snow',
+    fontFamily: 'Montserrat',
+    fontSize: '30px',
+  },
+  artistName:{
+    color: 'snow',
+    marginTop: '0.5%',
+    fontSize: '16px',
+    
+  }
 });
