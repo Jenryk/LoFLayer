237ded2f1943d72adb3521bba489a9b4a9b0eece
João Santana <jenryk9503@gmail.com>
2020-02-14 17:02:14 -0300

    Fixes the pause bug

--- components/Player/Album.js
+++ components/Player/Album.js
@@ -2,29 +2,63 @@ import React, {useState} from 'react';
 import {Image, View, TouchableOpacity} from 'react-native';
 import {styles} from './styles'
 import LofiGirl from '../../assets/images/lofi_girl.png';
-import images from '../img/index'
-export function Album() {
-    const [songState, setSongState] = useState(images.pause);
+import images from '../img/index';
+import {Audio} from 'expo-av'
+import { render } from 'react-dom';
+export class Album extends React.Component{
+    state = {
+        songState: images.play,
+        soundObject: null,
+        soundStatus: null
+    }
+    async componentDidMount(){
+        const { sound: soundObject, status } = await Audio.Sound.createAsync({
+            uri: 'http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3'
+        })
+        this.setState({
+            soundObject: soundObject,
+            soundStatus: status
+        })
+    }
+    onPressPlay = async () => {
+
+            let status = await this.state.soundObject.getStatusAsync()
+            if(this.state.songState == images.play && !this.state.soundStatus.isPlaying){
+                await this.state.soundObject.playAsync();
+                alert(status.isPlaying)
+                this.changeState()
+            }
+            else if(this.state.songState == images.pause){
+                await this.state.soundObject.stopAsync();
+                alert(status.isPlaying)
+                this.changeState()
+            }
+            
+            
+            
+            // Your soud is playing!
+          } 
     
 
-    return(
-        <View>
-            <Image source={LofiGirl} style={styles.albumStyle}></Image>
-            <TouchableOpacity style={styles.playerButton} onPress={changeState}>
-                <Image source={songState} style={styles.playerButton}></Image>
-            </TouchableOpacity>
-        </View>
-    )
-
-    function changeState(){
-        if(songState == images.pause){
-            setSongState(images.play)
+    changeState = () => {
+        if(this.state.songState == images.play){
+            this.setState({songState: images.pause})
         }
         else{
-            setSongState(images.pause)
+            this.setState({songState: images.play})
         }
-        
     }
+    render(){
+        
+        return (
+            <View>
+                <Image source={LofiGirl} style={styles.albumStyle}></Image>
+                    <TouchableOpacity onPress={this.onPressPlay}style={styles.playerButton}>
+                <Image source={this.state.songState}style={styles.playerButton}></Image>
+            </TouchableOpacity>
+            </View>)
+    }
+  
 
 }
 
